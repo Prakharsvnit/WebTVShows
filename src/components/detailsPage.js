c1ab86d003b14ff302ae961e8ee8887b0177665e
Prakharsvnit <tprakhar468@gmail.com>
2023-01-21 22:09:53 +0530

    Showdetails data recieved

---
+++ src/components/detailsPage.js
@@ -0,0 +1,5 @@
+const DetailsPage = () => {
+  return <></>;
+};
+
+export default DetailsPage;
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,39 @@
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
 import styles from "../css/header.module.css";
 
 const Header = () => {
+  const [searchShow, setSearchShow] = useState("");
+  const [showDetails, setShowDetails] = useState([]);
+  const inputRef = useRef(null);
+  const baseurl = "http://api.tvmaze.com/";
+
+  const fetchShowDetails = async (searchShow) => {
+    const response = await axios.get(`${baseurl}search/shows?q=${searchShow}`);
+    setShowDetails(response.data);
+  };
+
+  useEffect(() => {
+    fetchShowDetails(searchShow);
+  }, [searchShow]);
+
+  function handleSearch(event) {
+    event.preventDefault();
+    setSearchShow(event.target.value);
+    inputRef.current.focus();
+  }
+
   return (
     <header className={styles.header}>
       <p className={styles.tv}>TV Maze</p>
       <form className={styles.form}>
-        <input type="search" placeholder="Search" />
-        <button type="submit" className={styles.searchBtn}>
+        <input
+          ref={inputRef}
+          type="search"
+          placeholder="Search"
+          onChange={handleSearch}
+        />
+        <button onClick={fetchShowDetails} className={styles.searchBtn}>
           Go
         </button>
       </form>
